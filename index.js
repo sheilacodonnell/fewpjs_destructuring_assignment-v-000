8fbf9a6d3e6c0c50c236324df7c124dfa85289a1
Sheila O'Donnell <sheila@contexttravel.com>
2020-04-09 14:12:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const [black_and_white,, black, pink,,] = farmAnimals.split(' ')
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 const [red, orange, yellow, green, blue, indigo, violet] = colors
+const [r, o, y, g, b, i, v] = colors
+
 
 const muppet = {
   name: 'Kermit',
