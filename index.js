714ffca19a19ed7d8b325fffed228650d95a084b
Sheila O'Donnell <sheila@contexttravel.com>
2020-04-09 14:20:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ const k_muppet = {
 
 const {song_2, song_4} = k_muppet.k_album.the_muppet_movie
 const {k_color, k_job, k_partner} = k_muppet
+
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
